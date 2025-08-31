06114d01b64985b7bfb49a5029edf4a4834f5281
Dave Chen <yexinchen@gmail.com>
2025-08-31 17:19:26 -0400

    minor fix

--- simple_python/hooks/post_gen_project.py
+++ simple_python/hooks/post_gen_project.py
@@ -3,7 +3,7 @@ import subprocess
 
 def run_command():
     # Example: Run a shell command in the generated project directory
-    subprocess.run(["echo", "Hello from post_gen_project!"], check=True)
+    subprocess.run(["cmd", "/c", "echo Hello from post_gen_project!"], check=True)
 
 if __name__ == "__main__":
     run_command()
